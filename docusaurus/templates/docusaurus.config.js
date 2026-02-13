1947a603594f4d9527a2e25bcc58cb44acd87fa7
Gijs van der Meijde <gijs.van.der.meijde@t-hive.io>
2026-02-13 18:53:49 +0100

    making filepath a bit more robust

--- docusaurus/templates/docusaurus.config.js
+++ docusaurus/templates/docusaurus.config.js
@@ -1,5 +1,8 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
@@ -45,6 +48,7 @@ const config = {
       crossorigin: 'anonymous',
     },
   ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
