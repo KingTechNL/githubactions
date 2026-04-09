3cf1a985c47af43c327f982ead872afcb33e26d7
Gijs van der Meijde <gijs.van.der.meijde@t-hive.io>
2026-04-09 11:20:42 +0200

    Updated temporary script

--- docusaurus-build/template/disable-invalid-progress-plugin.js
+++ docusaurus-build/template/disable-invalid-progress-plugin.js
@@ -6,24 +6,31 @@ module.exports = function disableInvalidProgressPlugin() {
         return {};
       }
 
+      const originalCount = config.plugins.length;
       config.plugins = config.plugins.filter((plugin) => {
         if (!plugin || typeof plugin !== 'object') {
           return true;
         }
 
         const pluginLike = /** @type {{[key: string]: unknown}} */ (plugin);
+        const constructorName = plugin.constructor?.name || '';
         const hasWebpackBarShape =
-          ('reporter' in pluginLike || 'reporters' in pluginLike) &&
-          ('name' in pluginLike || 'color' in pluginLike);
+          constructorName === 'WebpackBar' ||
+          (('reporter' in pluginLike || 'reporters' in pluginLike) &&
+            ('name' in pluginLike || 'color' in pluginLike));
 
         if (hasWebpackBarShape) {
-          const pluginName = plugin.constructor?.name || 'unknown-plugin';
+          const pluginName = constructorName || 'unknown-plugin';
           console.warn(`Removing incompatible webpack progress plugin: ${pluginName}`);
           return false;
         }
 
         return true;
       });
+
+      if (config.plugins.length !== originalCount) {
+        console.log(`Filtered webpack plugins: ${originalCount} -> ${config.plugins.length}`);
+      }
 
       return {};
     },
